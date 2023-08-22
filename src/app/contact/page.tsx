c8230a9a8615974f9baf63135dcacb4c24fbfdac
dev-sandeepsingh <sandeepsingh@founderandlightning.com>
2023-08-22 17:28:49 +0530

    Added about us and contact pages

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+
+import SideNavBar from '../components/sideNavBar'
+
+export default function Home() {
+    return (
+        <div>
+            Contact us
+        </div>
+    )
+}
---
+++ src/app/users/page.tsx
@@ -0,0 +1,10 @@
+
+import SideNavBar from '../components/sideNavBar'
+
+export default function Home() {
+    return (
+        <div>
+            Users
+        </div>
+    )
+}
