52b80edae2c9dd21b375c7fa9a3d44b8ce441f0c
dev-sandeepsingh <sandeepsingh@founderandlightning.com>
2023-11-01 11:13:08 +0530

    Create component of sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,36 +2,13 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Sidebar from "@/app/components/sidebar";
+import Sidebar from "@/app/components/Sidebar";
 import { Fragment, useState } from 'react'
 import { Dialog, Menu, Transition } from '@headlessui/react'
-import {
-    Bars3Icon,
-    BellIcon,
-    CalendarIcon,
-    ChartPieIcon,
-    Cog6ToothIcon,
-    DocumentDuplicateIcon,
-    FolderIcon,
-    HomeIcon,
-    UsersIcon,
-    XMarkIcon,
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 
-const navigation = [
-    { name: 'Dashboard', href: '/', icon: HomeIcon, current: true },
-    { name: 'Team', href: '/Team', icon: UsersIcon, current: false },
-    { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-    { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-    { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
-    { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
-]
-const teams = [
-    { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
-    { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
-    { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
-]
+import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
+import {Bars3Icon, BellIcon, XMarkIcon} from "@heroicons/react/24/outline";
+
 const userNavigation = [
     { name: 'Your profile', href: '#' },
     { name: 'Sign out', href: '#' },
@@ -39,11 +16,7 @@ const userNavigation = [
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
-
 const inter = Inter({ subsets: ['latin'] })
-
-
-
 export default function RootLayout({
   children,
 }: {
@@ -107,90 +80,7 @@ export default function RootLayout({
           </Transition.Root>
 
           {/* Static sidebar for desktop */}
-          <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-              {/* Sidebar component, swap this element with another sidebar if you like */}
-              <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
-                  <div className="flex h-16 shrink-0 items-center">
-                      <img
-                          className="h-8 w-auto"
-                          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                          alt="Your Company"
-                      />
-                  </div>
-                  <nav className="flex flex-1 flex-col">
-                      <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                          <li>
-                              <ul role="list" className="-mx-2 space-y-1">
-                                  {navigation.map((item) => (
-                                      <li key={item.name}>
-                                          <a
-                                              href={item.href}
-                                              className={classNames(
-                                                  item.current
-                                                      ? 'bg-gray-50 text-indigo-600'
-                                                      : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
-                                                  'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                                              )}
-                                          >
-                                              <item.icon
-                                                  className={classNames(
-                                                      item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600',
-                                                      'h-6 w-6 shrink-0'
-                                                  )}
-                                                  aria-hidden="true"
-                                              />
-                                              {item.name}
-                                          </a>
-                                      </li>
-                                  ))}
-                              </ul>
-                          </li>
-                          <li>
-                              <div className="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
-                              <ul role="list" className="-mx-2 mt-2 space-y-1">
-                                  {teams.map((team) => (
-                                      <li key={team.name}>
-                                          <a
-                                              href={team.href}
-                                              className={classNames(
-                                                  team.current
-                                                      ? 'bg-gray-50 text-indigo-600'
-                                                      : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
-                                                  'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                                              )}
-                                          >
-                          <span
-                              className={classNames(
-                                  team.current
-                                      ? 'text-indigo-600 border-indigo-600'
-                                      : 'text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600',
-                                  'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'
-                              )}
-                          >
-                            {team.initial}
-                          </span>
-                                              <span className="truncate">{team.name}</span>
-                                          </a>
-                                      </li>
-                                  ))}
-                              </ul>
-                          </li>
-                          <li className="mt-auto">
-                              <a
-                                  href="#"
-                                  className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600"
-                              >
-                                  <Cog6ToothIcon
-                                      className="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600"
-                                      aria-hidden="true"
-                                  />
-                                  Settings
-                              </a>
-                          </li>
-                      </ul>
-                  </nav>
-              </div>
-          </div>
+          <Sidebar/>
 
           <div className="lg:pl-72">
               <div className="sticky top-0 z-40 lg:mx-auto lg:max-w-7xl lg:px-8">
