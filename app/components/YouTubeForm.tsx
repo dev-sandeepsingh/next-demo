29f9aeaa22fd3fb6d1ef2fa14e380924796a793e
dev-sandeepsingh <sandeepsingh@founderandlightning.com>
2023-12-08 15:21:47 +0530

    Submit form on button

--- app/components/YouTubeForm.tsx
+++ app/components/YouTubeForm.tsx
@@ -1,26 +1,38 @@
 'use client'
+import { useForm} from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
+type FormValues = {
+    username: string;
+    email: string;
+    channel: string;
+}
 
 export default function YouTubeForm() {
+    const form= useForm<FormValues>();
+    const { register, control, handleSubmit } = form;
+    const onSubmit = (data: FormValues) => {
+        console.log("Form submitted", data);
+    }
     return  (
         <div className='w-full'>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className='form-control p-3 w-2/5'>
                 <label className="w-1/3" htmlFor="username">User name</label>
-                <input className="border-2 ml-5 float-right" type="text" id="username" name="username"/>
+                <input className="border-2 ml-5 float-right" type="text" id="username" {...register("username")}/>
                 </div>
                 <div className='form-control p-3 w-2/5'>
                 <label className='className="w-1/3' htmlFor="email">Email</label>
-                <input type="text" className="border-2 ml-5 float-right" id="email" name="email"/>
+                <input type="text" className="border-2 ml-5 float-right" id="email" {...register("email")}/>
                 </div>
                 <div className='form-control p-3 w-2/5'>
                 <label className="w-1/3" htmlFor="channel">Channel</label>
-                <input type="text" className="border-2 ml-5 float-right" id="channel" name="channel"/>
+                <input type="text" className="border-2 ml-5 float-right" id="channel" {...register("channel")}/>
                 </div>
                 <div className='form-control p-3 w-2/5'>
                     <button className='float-right bg-green-500 p-5 border-2 border-r-amber-50'>Submit</button>
                 </div>
-
             </form>
+            <DevTool control={control} />
         </div>)
 }
